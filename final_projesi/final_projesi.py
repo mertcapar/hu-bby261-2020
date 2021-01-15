f5ad64e142318779acfbe8a55dea1fc1c94eabb0
mertcapar <61504190+mertcapar@users.noreply.github.com>
2021-01-15 11:57:54 +0300

    Update final_projesi.py

--- final_projesi/final_projesi.py
+++ final_projesi/final_projesi.py
@@ -4,28 +4,29 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
+#csv dosyasını okuttum
 veriler = pd.read_csv("ETH_USD_6ay.csv")
 
-# data frame dilimleme
+# verileri satır ve sütun şeklinde dilimledim
 x = veriler.iloc[:, 0:1]
 y = veriler.iloc[:, 1:]
 
-# numpy array donusumu
+# Numpy dizisine dönüştürdüm
 X = x.values
 Y = y.values
 
-# verilerin dagilimi
+# Numpy dizisindeki verileri grafik oluşturup, grafiğe aktardım
 plt.scatter(X,Y,color="orange")
 plt.xlabel("Gün")
 plt.ylabel("Fiyat")
 plt.title("Fiyat Değişim Grafiği")
 plt.show()
 
-# lineer regresyon
+# doğrusal artış çizgisini tanımlayıp X ve Y değişkenlerini fit fonksiyonu ile eğittim
 linreg = LinearRegression()
 linreg.fit(X, Y)
 
-# lineer regresyon gorsellestirme
+# doğrusal artış çizgisini grafiğe aktardım.
 plt.scatter(X, Y, color="orange")
 plt.plot(x, linreg.predict(X), color="green")
 plt.xlabel("Gün")
@@ -33,16 +34,16 @@ plt.ylabel("Fiyat")
 plt.title("Lineer Regresyon")
 plt.show()
 
-# lineer tahmin
+# grafikteki veriler ile doğrusal çizgideki kesişim noktalarına göre tahmin yapıyor (çizgi doğrusal olduğu için pek yararlı olmadı doğru tahmin etme olasığı düşük)
 print("Lineer Regresyon Tahmin:", linreg.predict([[230]]))
 
-# polinomsal regresyon
+# polinomsal çizgide açıyı en tutarlı olan 7 olarak belirledim ve fit fonksiyonu ile eğittim (iniş ve çıkışları gösterdiği için doğruluk payı daha yüksek)
 polreg = PolynomialFeatures(degree=7)
 xpol = polreg.fit_transform(X)
 linreg = LinearRegression()
 linreg.fit(xpol, y)
 
-# polinomsal regresyon gorsellestirme
+# polinomsal artış çizgisini grafiğe aktardım
 plt.scatter(X, Y, color="orange")
 plt.plot(X, linreg.predict(polreg.fit_transform(X)), color="green")
 plt.xlabel("Gün")
@@ -50,6 +51,8 @@ plt.ylabel("Fiyat")
 plt.title("Polinomsal Regresyon (Derece:7)")
 plt.show()
 
-# polinomsal tahmin
+# grafikteki veriler ile polinomsal çizgideki kesişim noktalarına göre tahmin yapıyor
 print("Polinomsal Regresyon Tahmin:", linreg.predict(polreg.fit_transform([[230]])))
 
+# tahminleri csv dosyasındaki verilere göre yapmaktadır. Verileri kullanarak eğitim gerçekleşiyor daha sonra o verilere göre tahmin yapıyor.
+# Örnek olarak 6 aylık verileri gösteren csv dosyasını import ettiğimizde 6.ayın son gününe kadar veriler bulunmakta. Bu tahmini 7.ayın ilk gününe göre yapmaktadır. 
